0c060901d200de0b07fc25280bcd869edf42191f
besaba <lazexe@yandex.ru>
2014-03-07 19:47:22 +0200

    menu added

--- src/testapplication/TestApplication.java
+++ src/testapplication/TestApplication.java
@@ -1,6 +1,9 @@
 package testapplication;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class TestApplication extends JFrame {
     
@@ -8,6 +11,22 @@ public class TestApplication extends JFrame {
         this.setTitle("Hello!");
         this.setSize(640, 480);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initMenu();
+    }
+    
+    void initMenu() {
+        JMenuBar bar = new JMenuBar();
+        JMenu menu1 = new JMenu("File");
+        JMenuItem item1 = new JMenuItem("Exit");
+        menu1.add(item1);
+        JMenu menu2 = new JMenu("Help");
+        JMenuItem item2 = new JMenuItem("About");
+        menu2.add(item2);
+        
+        bar.add(menu1);
+        bar.add(menu2);
+        this.setJMenuBar(bar);
+        
     }
 
     public static void main(String[] args) {
